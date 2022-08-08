21e8753333a3612658cbef8fd44fcd107ea52d55
Exreller <pipkonp@mail.ru>
2022-08-08 22:24:48 +0300

    Подбивка под дурацкий бинарный файл

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
-namespace Task4
+namespace FinalTask
 {
+    [Serializable]
+    class Student
+    {
+        public string Name { get; set; }
+        public string Group { get; set; }
+        public DateTime DateOfBirth { get; set; }
+       
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+
+            BinaryFormatter bf = new BinaryFormatter();
+           
+            // десериализация
+            using (var fs = new FileStream(@"C:\Users\Exreller\Desktop\NewFolder1\Students.dat", FileMode.OpenOrCreate))
+            {
+                Student[] students = (Student[]) bf.Deserialize(fs) ;
+                foreach(var student in students)
+                {
+                    Console.WriteLine("Объект десериализован");
+                    Console.WriteLine($"Имя: {student.Name} {student.Group} {student.DateOfBirth }");
+                }
+                
+            }
+            Console.ReadLine();
         }
     }
 }
