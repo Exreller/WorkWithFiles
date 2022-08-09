171f217b2fe0f749d2a38a5bd184597fd96aa130
Exreller <pipkonp@mail.ru>
2022-08-09 19:11:50 +0300

    Итоговые правки

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -10,29 +10,64 @@ namespace FinalTask
         public string Name { get; set; }
         public string Group { get; set; }
         public DateTime DateOfBirth { get; set; }
-       
+
     }
 
     class Program
     {
+
         static void Main(string[] args)
         {
-            
-
-            BinaryFormatter bf = new BinaryFormatter();
-           
-            // десериализация
-            using (var fs = new FileStream(@"C:\Users\Exreller\Desktop\NewFolder1\Students.dat", FileMode.OpenOrCreate))
+            try
             {
-                Student[] students = (Student[]) bf.Deserialize(fs) ;
-                foreach(var student in students)
+                Student[] students;
+                BinaryFormatter bf = new BinaryFormatter();
+                using (var fs = new FileStream(@"C:\Users\Exreller\Desktop\NewFolder1\Students.dat", FileMode.OpenOrCreate))
                 {
-                    Console.WriteLine("Объект десериализован");
-                    Console.WriteLine($"Имя: {student.Name} {student.Group} {student.DateOfBirth }");
+                    students = (Student[])bf.Deserialize(fs);
+                    foreach (var student in students)
+                    {
+                        Console.WriteLine($"Имя: {student.Name} {student.Group} {student.DateOfBirth }");
+                    }
+
                 }
-                
+
+                Console.WriteLine("Для продолжения нажмите любую клавишу");
+                Console.ReadLine();
+
+                string pathToStudents = @"C:\Users\Exreller\Desktop\NewFolder1\Students";
+                DirectoryInfo directoryStudents = new DirectoryInfo(pathToStudents);
+                if (!directoryStudents.Exists)
+                {
+                    directoryStudents.Create();
+                }
+
+                foreach (var student in students)
+                {
+                    string pathToGroups = $@"C:\Users\Exreller\Desktop\NewFolder1\Students\{student.Group}.txt";
+                    FileInfo group = new FileInfo(pathToGroups);
+                    if (!group.Exists)
+                    {
+                        using (StreamWriter sw = group.CreateText())
+                        {
+                            sw.Write(student.Name);
+                            sw.Write(student.DateOfBirth + Environment.NewLine);
+                        }
+                    }
+                    else
+                    {
+                        using (StreamWriter sw = group.AppendText())
+                        {
+                            sw.Write(student.Name);
+                            sw.Write(student.DateOfBirth + Environment.NewLine);
+                        }
+                    }
+                }
             }
-            Console.ReadLine();
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
